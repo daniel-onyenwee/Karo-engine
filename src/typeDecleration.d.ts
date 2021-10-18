78b763dc0898e790dbe0659155fc2ba554c2b1a4
daniel-rif <danielonyenwee@gmail.com>
2021-10-18 10:26:36 +0100

    ➕ add the CharacterTreeOption interface

--- src/typeDecleration.d.ts
+++ src/typeDecleration.d.ts
@@ -2,3 +2,9 @@ import { Game, Character } from "."
 
 export type CharacterParentType =  Character.Container|Game|Character.Arc|Character.Box|Character.Text|Character.Timer
 export type CharacterChildrenType = Character.Container|Character.Arc|Character.Box|Character.Text|Character.Timer
+export interface CharacterTreeOption {
+    name: string
+    path?: string
+    type: string
+    children: Map<string, CharacterTreeOption>
+} 
